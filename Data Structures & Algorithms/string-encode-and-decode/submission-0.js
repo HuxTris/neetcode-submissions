aea0515c19219ad94a5cd3d0ce5cb9d57a46a391
Phan Hữu Trí <146919973+HuxTris@users.noreply.github.com>
2026-04-13 11:53:31 +0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = '';
+
+        for ( let str of strs ) {
+            res += str.length + '#' + str;
+        }
+
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const res = [];
+
+        let i = 0;
+
+        while ( i < str.length ) {
+            let j = i;
+
+            while ( str[j] !== '#' ) {
+            j++;
+            }
+         const len = Number( str.slice(i, j) );
+
+        const s = str.slice( j + 1, j + 1 + len );
+
+        res.push(s);
+
+        i = j + 1 + len;
+
+        }
+       
+        return res;
+    }
+}
