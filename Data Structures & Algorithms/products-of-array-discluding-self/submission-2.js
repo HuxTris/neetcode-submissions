3fc399355ed79a59d6cf572ed88ad363200071f4
Phan Hữu Trí <146919973+HuxTris@users.noreply.github.com>
2026-04-14 11:21:10 +0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const len = nums.length;
+        const output = new Array(len).fill(1);
+
+        let prefix = 1;
+        for ( let i = 0 ; i < len ; i++ ) {
+            output[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        let suffix = 1;
+        for ( let i = len - 1 ; i >= 0 ; i-- ) {
+            output[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return output;
+    }
+}
